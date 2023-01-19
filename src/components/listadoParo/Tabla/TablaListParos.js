ef837e241077aba0011c4ac59faa0df54c809327
videvops <development@visolutions.com.mx>
2023-01-19 00:34:13 +0000

    Correcion Tabla

--- src/components/listadoParo/Tabla/TablaListParos.js
+++ src/components/listadoParo/Tabla/TablaListParos.js
@@ -19,7 +19,7 @@ const TablaListParos = ({ registros }) => {
                                 <Column field="linea" header="Linea" style={{ textAlign: "center" }} sortable/>
                                 <Column field="maquina" header="Maquina" style={{ textAlign: "center" }} sortable/>
                                 <Column field="modoFalla" header="Modo de Falla" style={{ textAlign: "center" }} sortable/>
-                                <Column field="incioParo" header="Inicio de Paro" style={{ textAlign: "center" }} sortable/>
+                                <Column field="inicioParo" header="Inicio de Paro" style={{ textAlign: "center" }} sortable/>
                                 <Column field="finParo" header="Fin de Paro" style={{ textAlign: "center" }} sortable/>
                                 <Column field="tiempo" header="Tiempo" style={{ textAlign: "center", width: '2rem' }} sortable />
                             </TablaDesing>
