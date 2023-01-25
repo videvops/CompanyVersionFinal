a746c61e65fb90b810fcf3933e10e233ce6dba8b
videvops <108509371+videvops@users.noreply.github.com>
2023-01-25 13:47:41 -0600

    Update CrearModificar.js

--- src/pages/Catalogos/ModoFalla/Dialogos/CrearModificar.js
+++ src/pages/Catalogos/ModoFalla/Dialogos/CrearModificar.js
@@ -1,10 +1,11 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
-import { Dropdown } from 'primereact/dropdown';
+// import { productDialogFooter } from '../Botones/CrearRegistro';
 import { productDialogFooter } from "../../ComponentsCat/Botones/CrearRegistro";
 
 const CrearModificar = ({ productDialog, titulos, hideDialog, product, updateField, saveProduct }) => {
+    console.log("creando componente");
     //--------------------| Validar campos  |--------------------
     const [validarNombre, setValidarNombre] = useState(""); // Validar nombre de planta
     const [boton, setBoton] = useState(false); // Activar o desactivar boton
@@ -20,58 +21,36 @@ const CrearModificar = ({ productDialog, titulos, hideDialog, product, updateFie
             setBoton(false);
         }
     };
- 
+
     //--------------------| Botones de confirmacion |--------------------
     //------> Botones para crear registro
     const crearRegistro = productDialogFooter(hideDialog, saveProduct, boton);
-    
+
     //--------------------| Valor que regresara  |--------------------
     return (
-        <> degugg </>
-    //     <Dialog 
-    //     visible={productDialog}
-    //     style={{ width: "450px" }}
-    //     header={titulos.VentanaCrear}
-    //     modal className="p-fluid"
-    //     footer={![product.idPlanta,product.modoFalla].includes('')&&crearRegistro}  
-    //     onHide={hideDialog}>
-    //          <div className="field">
-    //             <label>Planta</label>
-    //             <Dropdown 
-    //             optionLabel="planta" 
-    //             optionValue="id" 
-    //             value={product.idPlanta} 
-    //             options={plantasDisponibles} 
-    //             onChange={(e) => 
-    //                 {updateField(e.value, "idPlanta")}} 
-    //             placeholder="--Selecciona una planta--"
-    //             required
-    //             />
-    //         </div>
-    //         <div className="field">
-    //             {/* CAMBIAR.... */}
-    //             <label
-    //                 htmlFor="modoFalla" // CAMBIAR...
-    //             >
-    //                 Modo de Falla
-    //             </label>
-    //             <InputText
-    //                 id="modoFalla" // CAMBIAR...
-    //                 value={product.modoFalla} // CAMBIAR...
-    //                 onChange={(e) => {
-    //                     updateField(e.target.value.trim(), "modoFalla"); // CAMBIAR...
-    //                     Verificar(e.target.value);
-    //                 }}
-    //                 placeholder="--Escribe el nombre del modo de falla--"
-    //                 required
-    //                 autoFocus
-    //                 className={validarNombre}
-    //                 maxLength="30"
-    //             />
-    //         </div>
-    //         {boton && Advertencia}
-    //     </Dialog>
-    // 
+        <Dialog visible={productDialog} style={{ width: "450px" }} header={titulos.VentanaCrear} modal className="p-fluid" footer={crearRegistro} onHide={hideDialog}>
+            <div className="field">
+                {/* CAMBIAR.... */}
+                <label
+                    htmlFor="modoFalla" // CAMBIAR...
+                >
+                    Modo de Falla
+                </label>
+                <InputText
+                    id="modoFalla" // CAMBIAR...
+                    value={product.modoFalla} // CAMBIAR...
+                    onChange={(e) => {
+                        updateField(e.target.value.trim(), "modoFalla"); // CAMBIAR...
+                        Verificar(e.target.value);
+                    }}
+                    required
+                    autoFocus
+                    className={validarNombre}
+                    maxLength="30"
+                />
+                {boton && Advertencia}
+            </div>
+        </Dialog>
     );
 };
 
